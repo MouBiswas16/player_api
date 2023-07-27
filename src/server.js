b3fa3d4c17f790db05cf72d28ce1763cde7f2f54
Mou Biswas <qtmeaowmou16@gmail.com>
2023-07-28 03:21:45 +0600

    completion

--- src/server.js
+++ src/server.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const cors = require("cors");
 const morgan = require("morgan");
+const shortid = require("shortid");
+const fs = require("fs/promises");
+const path = require("node:path")
+const dbLocation = path.resolve("src", "data.json");
 
 const app = express();
 
@@ -18,6 +22,115 @@ app.use(express.json());
  * PATCH   - /:id     - update player
  * DELETE  - /:id     - delete player from db
  */
+
+//Example :
+
+/**
+ * GET      /products       find all variable products from the system
+ * POST     /products       create new product
+ * GET      /products/id    find a single product
+ * PUT      /products/id    update a single product
+ * PATCH    /products/id    update a single product
+ * DELETE   /products/id    delete a single product
+ */
+
+app.delete("/:id", async (req, res) => {
+    const id = req.params.id;
+
+    const data = await fs.readFile(dbLocation);
+    const players = JSON.parse(data);
+    let player = players.find((item) => item.id == id);
+
+    if (!player) {
+        return res.status(404).json({message: "Player Not Found"});
+    }
+
+    const newPlayers = players.filter(item => item.id != id);
+    await fs.writeFile(dbLocation, JSON.stringify(newPlayers));
+    res.status(204).send();
+
+});
+
+app.put("/:id", async (req, res) => {
+ const id = req.params.id;
+
+ const data = await fs.readFile(dbLocation);
+ const players = JSON.parse(data);
+ let player = players.find((item) => item.id == id);
+
+ if (!player) {
+    player = {
+        ...req.body,
+        id: shortid.generate(),
+    };
+    players.push(player);
+ }else{
+    player.name = req.body.name;
+    player.country = req.body.country;
+    player.rank = req.body.rank;
+ }
+
+ await fs.writeFile(dbLocation, JSON.stringify(players));
+ res.status(200).json(player);
+
+});
+
+app.patch("/:id", async (req, res) => {
+ const id = req.params.id;
+ 
+ const data = await fs.readFile(dbLocation);
+ const players = JSON.parse(data);
+ const player = players.find((item) => item.id == id);
+
+ if (!player) {
+    return res.status(404).json({message: "Player Not Found"});
+ }
+
+ player.name = req.body.name || player.name;
+ player.country = req.body.country || player.country;
+ player.rank = req.body.rank || player.rank;
+
+ await fs.writeFile(dbLocation, JSON.stringify(players));
+ res.status(200).json(player);
+
+});
+
+app.get("/:id", async (req, res) => {
+    const id = req.params.id;
+
+    const data = await fs.readFile(dbLocation);
+    const players = JSON.parse(data);
+
+    const player = players.find((item) => item.id == id);
+
+    if (!player) {
+        return res.status(404).json({message: "Player Not Found"});
+    }
+
+    res.set("Cache-control", "public, max-age=300");
+    res.status(200).json(player);
+});
+
+app.post("/", async(req, res) => {
+    const player = {
+        ...req.body,
+        id: shortid.generate(),
+    };
+    
+    const data = await fs.readFile(dbLocation);
+    const players = JSON.parse(data);
+    players.push(player);
+
+    await fs.writeFile(dbLocation, JSON.stringify(players));
+    res.status(201).json(player);
+});
+
+app.get("/", async (req, res) => {
+    const data = await  fs.readFile(dbLocation);
+    const players = JSON.parse(data);
+    res.set("Cache-control", "public, max-age=300");
+    res.status(201).json(players);
+})
 
 app.get("/health",(_req, res) => {
     res.status(200).json({status: "OK"});
